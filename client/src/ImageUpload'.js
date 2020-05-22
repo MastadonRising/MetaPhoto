835c64c9df1f95a1b12226bab98f5d45f760771b
Gus (Nala) <gusrosales7@gmail.com>
2020-05-21 19:04:09 -0700

    set up axios call to mountain climbing API

--- client/src/ImageUpload'.js
+++ client/src/ImageUpload'.js
@@ -1,25 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import EXIF from "exif-js";
+import Axios from "axios";
 
 function ImageUploadx() {
   const [exifTags, setExifTags] = useState({});
-  console.log(exifTags);
+  const [currentGPS, setCurrentGPS] = useState({ lat: 38.417, lon: -121.463 });
+  const [climbRoutes, setClimbRoutes] = useState([]);
+
+  useEffect(() => {
+    const queryURI = `https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=${currentGPS.lat}&lon=${currentGPS.lon}&maxDistance=30&key=200765490-6a4f3ccdce84ab9b6225f209a2b16baf`;
+    console.log(exifTags);
+    Axios.get(queryURI).then((response) => {
+      setClimbRoutes(response.data.routes);
+      // console.log(response);
+    });
+    console.log(climbRoutes);
+  }, [currentGPS]);
 
   function handleChange({
     target: {
       files: [file],
     },
   }) {
-
     // save it to cdn here
     //
     //
-
 
     if (file && file.name) {
       EXIF.getData(file, function () {
         // var exifData = EXIF.pretty(this);
         if (this) {
+          setCurrentGPS({ lat: 0, lon: 0 });
           console.log(this);
           setExifTags({ data: this.exifdata });
         } else {
@@ -31,15 +42,15 @@ function ImageUploadx() {
 
   function convertToDecimalDeg(ref, array) {
     let result = (array[0] + array[1] / 60 + array[2] / 3600).toFixed(4);
-    
-    if(array !==undefined){
+
+    if (array !== undefined) {
       if (ref === (`N` || `E`)) {
         return result;
       } else {
         return -result;
       }
     }
-    return `no data`
+    return `no data`;
   }
 
   return (
@@ -75,11 +86,21 @@ function ImageUploadx() {
           <tr>
             <td>Make/Model</td>
             <td>
-              {exifTags.data ? exifTags.data.Make + '/' + exifTags.data.Model : `No Make/Model Found`}
+              {exifTags.data
+                ? exifTags.data.Make + "/" + exifTags.data.Model
+                : `No Make/Model Found`}
             </td>
           </tr>
         </tbody>
       </table>
+      <div>
+        <h4>Climbs Near You</h4>
+        <ul>
+          {climbRoutes.map((i) => (
+            <li>{i.name}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
