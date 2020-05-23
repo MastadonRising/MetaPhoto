fc31bea7d3ed08bbb765212ca3dcdf3d165fceb6
luis Sandoval <luissandoval1126@gmail.com>
2020-05-23 10:59:16 -0700

    fixed resource model field names

--- models/resources.js
+++ models/resources.js
@@ -3,8 +3,9 @@ const Schema = mongoose.Schema;
 
 const ResourceSchema = new Schema({
   name: { type: String, required: true },
+  url: { type: String },
   photo: { type: String, required: true },
-  link: { type: String },
+  desc: { type: String },
 });
 
 const Resource = mongoose.model("Resource", ResourceSchema);
