3117354c684c4d14fe783022952bae84c57c55b5
luis Sandoval <luissandoval1126@gmail.com>
2020-05-27 20:27:51 -0700

    added local climbs object structure in comments in explorer page

--- client/src/pages/explore.js
+++ client/src/pages/explore.js
@@ -28,3 +28,25 @@ function Explore() {
 }
 
 export default Explore;
+
+// localClimbs  is an array of objects
+// strucutre below
+
+// imgSqSmall are small square images the others are wonky sizes
+// {
+// id: 112840319
+// imgMedium: "https://cdn2.apstatic.com/photos/climb/114032367_medium_1516661975.jpg"
+// imgSmall: "https://cdn2.apstatic.com/photos/climb/114032367_small_1516661975.jpg"
+// imgSmallMed: "https://cdn2.apstatic.com/photos/climb/114032367_smallMed_1516661975.jpg"
+// imgSqSmall: "https://cdn2.apstatic.com/photos/climb/114032367_sqsmall_1516661975.jpg"
+// latitude: 38.682
+// location: (6) ["California", "Lake Tahoe", "Highway 50 Corridor", "Folsom", "Rainbow Boulders", "Area B"]
+// longitude: -121.1759
+// name: "The Easter Egg"
+// pitches: ""
+// rating: "V4"
+// starVotes: 3
+// stars: 4.7
+// type: "Boulder"
+// url: "https://www.mountainproject.com/route/112840319/the-easter-egg"
+// }
