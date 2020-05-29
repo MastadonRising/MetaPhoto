f3865c5c53e04ec7bcf844d2cf9934e9e8c51643
luis Sandoval <luissandoval1126@gmail.com>
2020-05-28 21:17:42 -0700

    server routes updated for all tables

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,44 @@
 const router = require("express").Router();
-const photoRoutes = require("./api/photos");
-const resourcesRoutes = require("./api/resources");
-const routesRoutes = require("./api/routes");
+const routesController = require("../controllers/routesController");
 const resourcesController = require("../controllers/resourcesController");
+const photoController = require("../controllers/photosController");
 const userController = require("../controllers/userController");
 
-router.use("/photo", photoRoutes);
-router.use("/api/resources", resourcesRoutes);
-router.use("/routes", routesRoutes);
+router.route("/api/resources").get(resourcesController.findAll);
 
-// router.route("/api/resources").get(resourcesController.findAll);
+router
+  .route("/api/photo")
+  .get(photoController.findAll)
+  .post(photoController.create);
+
+router
+  .route("/api/photo:id")
+  .post(photoController.like)
+  .get(photoController.findAll)
+  .put(photoController.update)
+  .delete(photoController.remove);
+
+router
+  .route("/api/resources")
+  .get(resourcesController.findAll)
+  .post(resourcesController.create);
+router
+  .route("/api/resources:id")
+  .put(resourcesController.update)
+  .delete(resourcesController.remove);
+
+router.route("/routes/api:id").get(routesController.findAll);
+
+router
+  .route("/api/user")
+  .get(userController.findAll)
+  .post(userController.createUser);
+
+router
+  .route("/api/user:id")
+  .get(userController.findById)
+  .put(userController.updateUser)
+  .delete(userController.removeUser);
+router.route("/api/favorite:id").post(userController.addFavorite);
 
 module.exports = router;
