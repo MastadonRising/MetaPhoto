f95f3719249ff2fc9c7042885de0ae816785dfe5
luis Sandoval <luissandoval1126@gmail.com>
2020-05-30 13:41:46 -0700

    erging in Quinn's work

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,10 +6,21 @@ export default {
     const queryURI = `https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=${GPS.coords.latitude}&lon=${GPS.coords.longitude}&maxDistance=30&key=200765490-6a4f3ccdce84ab9b6225f209a2b16baf`;
     return axios.get(queryURI);
   },
+  getRoutesbySearch: function (searchTerm) {
+    const geoCodeKey = "jmu2hzM4mHMBWSGPseb1cGFiAZ4CSPKI";
+    const url =
+      "http://www.mapquestapi.com/geocoding/v1/address?key=" +
+      geoCodeKey +
+      "&location=" +
+      searchTerm.toLowerCase();
+    return axios.get(url);
+  },
   getResources: function () {
     return axios.get("/api/resources");
   },
-
+  getPhoto: function () {
+    return axios.get("/api/photo");
+  },
   postPhoto: function (data) {
     data.filesUploaded.forEach((photo) => {
       console.log(photo);
@@ -30,7 +41,6 @@ export default {
 
   postLike: function (data) {
     let Like = {
-      likeID: data.likeID,
       type: data.type,
       userID: data.userID,
     };
