72ab76bd13282af91dbf0ceeb2c1f69b4cc4fc05
Gus (Nala) <gusrosales7@gmail.com>
2020-06-06 14:54:08 -0700

    pre-merge master

--- client/src/App.js
+++ client/src/App.js
@@ -25,24 +25,7 @@ export default function App() {
   return (
     <Router>
       <Container>
-<<<<<<< HEAD
-        <nav style={{ float: "left" }}>
-          <Menu vertical>
-            <Menu.Item as={Link} to='/' name='Home'/>
-            <Menu.Item as={Link} to="/explore" name="Explore" />
-            <Menu.Item as={Link} to="/myaccount" name="My Account" />
-            <Menu.Item as={Link} to="/resources" name='Resources' />
-            <Menu.Item as={Link} to="/upload" name='Upload' />
-            {(state.loggedIn) ? <Menu.Item as={Button} content='Logout' onClick={() => API.logout().then(res => console.log(res))} /> : null }
-          </Menu>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-=======
            <Switch>
->>>>>>> 833355d29e373542ed082a3bd0f6e17268464f69
         <Route exact path={"/"}>
             <Home {...state} stateChanger={updateLogIn} />
           </Route>
