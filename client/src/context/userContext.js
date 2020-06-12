cf80ae76600c9b07cdf36a44834aa30ccf508686
luis Sandoval <luissandoval1126@gmail.com>
2020-06-11 20:00:18 -0700

    logout route refreshes properly

--- client/src/context/userContext.js
+++ client/src/context/userContext.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const UserContext = React.createContext({
+  _id: "",
   userName: "",
+  firstName: "",
+  lastName: "",
+  email: "",
+  favorites: [],
   Login: () => {},
 });
 
