cb2d12b230c32eda4c0bd07cace72df8ee2921d5
luis Sandoval <luissandoval1126@gmail.com>
2020-06-16 20:28:55 -0700

    fixed connection String

--- server.js
+++ server.js
@@ -52,7 +52,8 @@ app.use(routes);
 // });
 
 mongoose.connect(
-  process.env.MONGODB_URI || mongodb:user:password1@ds031948.mlab.com:31948/heroku_tmwz9gv0,
+  process.env.MONGODB_URI ||
+    "mongodb://user:password1@ds031948.mlab.com:31948/heroku_tmwz9gv0",
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 app.listen(PORT, function () {
